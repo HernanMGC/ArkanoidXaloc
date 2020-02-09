f097ae6d6f83ad5d172d3acbb76bd3cc0033a549
Hernán <hernan.gonzalez.calderon@gmail.com>
2020-02-09 19:39:04 +0100

    Level creation by designer choice. Ball's control with racket bettered.

---
+++ Assets/Scripts/Editor/ArkanoidLevelDrawer.cs
@@ -0,0 +1,27 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CustomPropertyDrawer(typeof(ArkanoidLevel))]
+public class ArkanoidLevelDrawer : PropertyDrawer
+{
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    {
+        int oldIndentLevel = EditorGUI.indentLevel;
+        label = EditorGUI.BeginProperty(position, label, property);
+        Rect contentPosition = EditorGUI.PrefixLabel(position, label);
+        position.height = 100f;
+        EditorGUI.indentLevel += 1;
+        contentPosition = EditorGUI.IndentedRect(position);
+        contentPosition.y += 18f;
+        EditorGUI.indentLevel = 0;
+        EditorGUIUtility.labelWidth = 50f;
+        EditorGUI.PropertyField(position, property.FindPropertyRelative("levelDefinition"));
+        EditorGUI.EndProperty();
+        EditorGUI.indentLevel = oldIndentLevel;
+    }
+
+    public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+    {
+        return 120f;
+    }
+}
--- Assets/Scripts/MonoBehaviour/GameManager.cs
+++ Assets/Scripts/MonoBehaviour/GameManager.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections;
+﻿using System.IO;
+using System.Linq;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -13,17 +15,18 @@ public class GameManager : MonoBehaviour
     public float capsuleDropProbability;
     public CapsulePrize[] capsulePrizes;
 
-    // Start is called before the first frame update
-    void Start()
+    [Header("LevelSettings")]
+    public GameObject brickStartingPoint;
+    public Brick brickPrefab;
+    public ArkanoidLevel[] arkanoidLevels;
+    public float vBrickOffset;
+    public float hBrickOffset;
+
+    private void Start()
     {
-        
+        InitializeLevel();    
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     public void BrickDestroyed(Transform brickTransform) {
         Debug.Log("BrickDestroyed");
@@ -47,6 +50,74 @@ public class GameManager : MonoBehaviour
             }
 
             var go = Instantiate(capsulePrizes[i].capsule.gameObject, brickTransform.position, Quaternion.identity) as GameObject;
+        }
+    }
+
+    private void InitializeLevel()
+    {
+        string levelDefinition = arkanoidLevels[0].levelDefinition.ToUpper();
+
+        Debug.Log(arkanoidLevels[0].levelDefinition);
+
+        char[] delims = new[] { '\r', '\n' };
+        string[] levelDefinitionArray = levelDefinition.Split(delims, System.StringSplitOptions.RemoveEmptyEntries);
+
+        float vBrickPosition = 0.0f;
+        float hBrickPosition = 0.0f;
+
+        for (int i = 0; i < levelDefinitionArray.Length; i++)
+        {
+            vBrickPosition = i * vBrickOffset;
+            string line = levelDefinitionArray[i].Substring(0, Mathf.Min(levelDefinitionArray[i].Length, 10));
+
+            for (int j = 0; j < line.Length; j++)
+            {
+                hBrickPosition = j * hBrickOffset;
+                Vector3 newBrickPosition = new Vector3(brickStartingPoint.transform.position.x + hBrickPosition, brickStartingPoint.transform.position.y - vBrickPosition, 0f);
+
+                InstantiateBrick(line[j], newBrickPosition);
+            }
+        }
+    }
+
+    private void InstantiateBrick(char brickChar, Vector3 position) {
+        GameObject go;
+
+        switch (brickChar)
+            {
+                case 'U':
+                    go = Instantiate(brickPrefab.gameObject, position, Quaternion.identity) as GameObject;
+                    go.GetComponent<Brick>().hitableReaction = Hitable.HitableReaction.DoNothing;
+                    go.GetComponent<SpriteRenderer>().sprite = this.unbreakableBrickPic;
+                    go.SetActive(true);
+                    break;
+
+                case '1':
+                    go = Instantiate(brickPrefab.gameObject, position, Quaternion.identity) as GameObject;
+                    go.GetComponent<Brick>().hitableReaction = Hitable.HitableReaction.DestroySelf;
+                    go.GetComponent<Brick>().durability = 1;
+                    go.GetComponent<SpriteRenderer>().sprite = this.breakableBrickPics[Mathf.Min(0, this.breakableBrickPics.Length - 1)];
+                    go.SetActive(true);
+                    break;
+
+                case '2':
+                    go = Instantiate(brickPrefab.gameObject, position, Quaternion.identity) as GameObject;
+                    go.GetComponent<Brick>().hitableReaction = Hitable.HitableReaction.DestroySelf;
+                    go.GetComponent<Brick>().durability = 2;
+                    go.GetComponent<SpriteRenderer>().sprite = this.breakableBrickPics[Mathf.Min(1, this.breakableBrickPics.Length - 1)];
+                    go.SetActive(true);
+                    break;
+
+                case '3':
+                    go = Instantiate(brickPrefab.gameObject, position, Quaternion.identity) as GameObject;
+                    go.GetComponent<Brick>().hitableReaction = Hitable.HitableReaction.DestroySelf;
+                    go.GetComponent<Brick>().durability = 3;
+                    go.GetComponent<SpriteRenderer>().sprite = this.breakableBrickPics[Mathf.Min(2, this.breakableBrickPics.Length - 1)];
+                    go.SetActive(true);
+                    break;
+
+                default:
+                    break;
         }
     }
 }
